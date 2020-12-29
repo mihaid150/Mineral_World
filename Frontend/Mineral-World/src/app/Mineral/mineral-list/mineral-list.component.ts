b9b16777287c6aef05cbad2635f7fce773d89710
Mihai <mdaian150@yahoo.com>
2020-12-29 10:02:11 +0200

    mineral.6

--- Frontend/Mineral-World/src/app/Mineral/mineral-list/mineral-list.component.ts
+++ Frontend/Mineral-World/src/app/Mineral/mineral-list/mineral-list.component.ts
@@ -8,57 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MineralListComponent implements OnInit {
 
-  minerals:Array<any> =
-  [
-    {
-        "Id":1,
-        "Name":"Aur 22k",
-        "Formula":"Au",
-        "Price":"100 RON",
-        "Cantitate":"20"
-    },
-    {
-      "Id":2,
-      "Name":"Cuarț Roz",
-      "Formula":"SiO2",
-      "Price":"80 RON",
-      "Cantitate":"20 buc"
-    },
-    {
-      "Id":3,
-      "Name":"Ochi Tigru",
-      "Formula":"SiO2",
-      "Price":"200 RON",
-      "Cantitate":"10 buc"
-    },
-    {
-      "Id":4,
-      "Name":"Ametist",
-      "Formula":"SiO2 Fe+3",
-      "Price":"150 RON",
-      "Cantitate":"15 buc"
-    },
-    {
-      "Id":5,
-      "Name":"Pirită",
-      "Formula":"FeS2",
-      "Price":"50 RON",
-      "Cantitate":"30 buc"
-    }
-  ]
+  minerals!: any;
 
 
-  constructor() { }
 
-  ngOnInit(): void {
-    // this.http.get('data/minerals.json').subscribe(
-    // data=>
-    // {
-    //   this.minerals=data;
-    //   console.log(data)}
+  constructor(private http:HttpClient) { }
 
-    //   );
+  ngOnInit(): void
+  {
+     this.http.get('data/minerals.json').subscribe(data=>
+      {
+        this.minerals=data;
+        console.log(data);
+      });
 
-
-}
+  }
 }
