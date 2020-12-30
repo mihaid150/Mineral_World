722ce279826434f5bdbc64b7df9ab1b7cfc60fdf
Mihai <mdaian150@yahoo.com>
2020-12-30 22:55:17 +0200

    mineral.12

--- Frontend/Mineral-World/src/app/user/user-register/user-register/user-register.component.ts
+++ Frontend/Mineral-World/src/app/user/user-register/user-register/user-register.component.ts
@@ -19,6 +19,27 @@ export class UserRegisterComponent implements OnInit {
      mobile: new FormControl(null, [Validators.required, Validators.maxLength(10)])
     });
   }
+
+get userName(){
+  return this.registerationForm!.get('userName') as FormControl;
+}
+
+get password(){
+  return this.registerationForm!.get('password') as FormControl;
+}
+
+get confirmPassword(){
+  return this.registerationForm!.get('confirmPassword') as FormControl;
+}
+
+get email(){
+  return this.registerationForm!.get('email') as FormControl;
+}
+
+get mobile(){
+  return this.registerationForm!.get('userName') as FormControl;
+}
+
  onSubmit(){
    console.log(this.registerationForm);
  }
